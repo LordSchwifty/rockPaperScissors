0f824d3079f178cac2ee50004fd747d1ce8fe736
Tom Doder <tdoder89@gmail.com>
2023-01-14 15:06:24 -0600

    feat: add playability functions to the DOM and set timeout

--- scissors/scripts.js
+++ scissors/scripts.js
@@ -10,6 +10,8 @@ var lightsaberIcon = document.getElementById('lightsaber')
 var currentGame = new Game()
 var humanScore = document.querySelector('.human-score')
 var computerScore = document.querySelector('.computer-score')
+var selectPlayer = document.getElementById('selection')
+
 //event listeners
 classicBtn.addEventListener('click', classicGameSelection)
 difficultBtn.addEventListener('click', difficultGameSelection)
@@ -57,8 +59,21 @@ function chooseIcon(event){
    console.log(event.target.id)
    currentGame.standoff()
    updateScore()
+   updateWinner()
+   setTimeout(resetPage, 2000)
 }
 function updateScore() {
     computerScore.innerText = currentGame.computer.wins
     humanScore.innerText = currentGame.human.wins
+}
+
+function updateWinner() {
+    if(currentGame.winner === 'draw') {
+    selectPlayer.innerText = 'draw'
+  } else {
+    selectPlayer.innerText = `${currentGame.winner.token} WINS!!!`
+  }
+}
+function resetPage() {
+    selectPlayer.innerText = 'Select Your Fighter'
 }
