df0aa80f8a87383d03c2eca87e7aba28ccbb0350
Tom Doder <tdoder89@gmail.com>
2023-01-14 10:15:21 -0600

    show changes to standoff function in game class

--- scissors/game.js
+++ scissors/game.js
@@ -15,6 +15,27 @@ selectGameType(event) {
         this.choice = ["rock", "paper", "scissors", "raygun", "lightsaber"]
 }
 standoff() {
+    if(this.human.pick === "rock" && this.computer.pick === "scissors") {
+        this.winner = this.human
+        this.human.wins += 1
+    } else if(this.computer.pick === "rock" && this.human.pick === "scissors") {
+        this.winner = this.computer
+        this.computer.wins += 1
+    } else if(this.human.pick === "paper" && this.computer.pick === "rock") {
+        this.winner = this.human
+        this.human.wins +=1
+    } else if (this.computer.pick === "paper" && this.human.pick === "rock") {
+        this.winner = this.computer
+        this.computer.wins += 1
+    } else if(this.human.pick === "scissors" && this.computer.pick === "paper") {
+        this.winner = this.human
+        this.human.wins += 1
+    } else if(this.computer.pick === "scissors" && this.human.pick === "paper") {
+        this.winner = this.computer
+        this.computer.wins += 1
+    } else if(this.computer.pick === this.human.pick) {
+        return `Draw!`
+    }
 
 }
 }
